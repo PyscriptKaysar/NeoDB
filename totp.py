4f064653bc31705f080bf7048ad839c131116631
oscarwongjh <oscarwong061128@gmail.com>
2024-12-11 21:03:28 +0800

    2fa draft 2

--- totp.py
+++ totp.py
@@ -1,5 +1,7 @@
 import pyotp
-import QRCode
+import qrcode
+
+
 
 #one time thing method for one users register account, give them qr for them to scan in google auth app
 
@@ -7,13 +9,7 @@ def generate_key():
     user_key = pyotp.random_base32()
     return user_key
 
-def generate_URI() -> URI:
-    user_key = generate_key()
-    otp = get_otp(user_key)
-    URI = otp.provisioning_uri(otp)
-    print(otp)
-    print(URI)
-    return URI
+
 
 def verify_otp(google_otp, user_key):
     otp = get_otp(user_key)
@@ -23,5 +19,16 @@ def get_otp(user_key):
     otp = pyotp.TOTP(user_key)
     return otp
 
-secret_key = generate_key()
-print(generate_URI(secret_key))
+
+
+temp_user_key = generate_key()
+def generate_URI() -> URI:
+    otp = get_otp(temp_user_key)
+    URI = otp.provisioning_uri(otp)
+    print(otp)
+    print(URI)
+    return URI
+
+def create_qr(URI):
+    img = qrcode.make(generate_URI())
+    return img
